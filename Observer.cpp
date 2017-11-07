192ec5d62e657062f2748d5bddd77e0082077769
cho <cuhcuh1@phill-it.com>
2017-11-07 17:23:03 +0900

    fix Observer Pattern

--- Observer.cpp
+++ Observer.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cstdlib>
 #include <unistd.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -19,8 +20,8 @@ class SubMachine: public Element {
     int limit;
     bool *flag;
 public:
-    SubMachine(bool *flag) {
-        num=0;
+    SubMachine(bool *flag, int initNum=0) {
+        num=initNum;
         limit=0x80000000;
         this->flag=flag;
     }
@@ -38,7 +39,7 @@ public:
                 delete this;
                 return (void*)0;
             }
-            printf("[%lu]thread num is [%d]\n", pthread_self(), num);
+            printf("[%lu]thread num is [%4d]\n", pthread_self(), num);
         }
     }
     virtual int posNum() {return num;}
@@ -52,20 +53,39 @@ class PrimeOne {
     vector<Element*> list;
     vector<pthread_t*> thList;
     vector<bool*> flag;
-    void del(int index) {
-        Element *get=list[index];
-        pthread_t *getThread=thList[index];
-        list.erase(list.begin()+index);
-        thList.erase(thList.begin()+index);
-        pthread_cancel(*getThread);
-        delete getThread;
-        delete get;
+    pthread_t watchThread;
+    static int getAvailList(vector<bool*> *flag) {
+        int ret=0;
+        for(int i=0; i<flag->size(); i++) {
+            if(!(*flag->at(i))) ret++;
+        }
+        return ret;
     }
 public:
-    void add() {
+    static void* watching(void *arg) {
+        int availSize=getAvailList(static_cast<vector<bool*>* >(arg));
+        while(true) {
+            sleep(0.5);
+            int posSize=getAvailList(static_cast<vector<bool*>* >(arg));
+            if(posSize==1) {
+                availSize=posSize;
+                printf("Element Size [%d]\n", availSize);
+                break;
+            } else if(availSize!=posSize) {
+                availSize=posSize;
+                printf("Element Size [%d]\n", availSize);
+            }
+        }
+    }
+    void stop() {
+        for(int i=0; i<list.size(); i++) {
+            pthread_cancel(*thList[i]);
+        }
+    }
+    void add(int initNum) {
         bool *f=new bool(false);
         flag.push_back(f);
-        Element *in=new SubMachine(f);
+        Element *in=new SubMachine(f, initNum);
         pthread_t *tmpThread=new pthread_t;
         pthread_create(tmpThread, NULL, SubMachine::runHelper, in);
         list.push_back(in);
@@ -77,31 +97,10 @@ public:
                 list[i]->update(limit);
             }
         }
-        for(int i=0; i<list.size(); i++) {
-            if(*flag[i]) {
-                delete flag[i];
-                flag.erase(flag.begin()+i);
-                list.erase(list.begin()+i);
-                delete thList[i];
-                thList.erase(thList.begin()+i);
-                flag.erase(flag.begin()+i);
-                i--;
-            }
-        }
     }
-    void cut(int limit) {
-        for(int i=0; i<list.size(); i++) {
-            int num=list[i]->posNum();
-            if(num<limit) {
-                printf("[%d] index is less than limit[limit>%d]. Delete it\n", i, num);
-                del(i);
-            }
-        }
+    PrimeOne() {
+        pthread_create(&watchThread, NULL, PrimeOne::watching, &flag);
     }
-    int getSize() {
-        return list.size();
-    }
-    PrimeOne() {}
     PrimeOne(int n) {
         for(int i=0; i<n; i++) {
             bool *f=new bool(false);
@@ -112,11 +111,20 @@ public:
             list.push_back(tmp);
             thList.push_back(tmpThread);
         }
+        pthread_create(&watchThread, NULL, PrimeOne::watching, &flag);
     }
     ~PrimeOne() {
+        pthread_cancel(watchThread);
+        int lastOne=0x80000000;
         for(int i=0; i<list.size(); i++) {
-            del(i);
+            if(!(*flag[i])) {
+                lastOne=max(lastOne, list[i]->posNum());
+                delete list[i];
+            }
+            delete thList[i];
+            delete flag[i];
         }
+        printf("LastOne Number[%d]\n", lastOne);
     }
 };
 
@@ -126,10 +134,11 @@ int main() {
     printf("Element Start Size is [%d]\n", init);
     printf("Main Thread[%lu]\n", pthread_self());
     for(int i=0; i<5; i++) {
+        if(i%2) p.add(i*20);
         sleep(60);
         printf("Limit [%d]\n", i*10);
         p.limitUpdate(i*10);
-        printf("Element Size is [%d]\n", p.getSize());
     }
+    p.stop();
     return 0;
 }
